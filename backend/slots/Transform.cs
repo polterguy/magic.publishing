e68826fa70bbb22e9a36698029dac7c3afb0e94f
Thomas Hansen <thomas@gaiasoul.com>
2020-02-22 20:52:09 +0200

    Update Transform.cs

--- backend/slots/Transform.cs
+++ backend/slots/Transform.cs
@@ -4,12 +4,15 @@
  */
 
 using System;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Globalization;
 using Markdig;
 using magic.node;
 using magic.node.extensions;
 using magic.signals.contracts;
+using magic.node.extensions.hyperlambda;
 
 namespace backend.slots
 {
@@ -28,13 +31,16 @@ namespace backend.slots
         public void Signal(ISignaler signaler, Node input)
         {
             var template = input.Children.First(x => x.Name == "template").Get<string>();
-            var created = input.Children.First(x => x.Name == "created").Get<DateTime>();
+            var content = input.Children.First(x => x.Name == "content").Get<string>();
             var title = input.Children.First(x => x.Name == "title").Get<string>();
             var author = input.Children.First(x => x.Name == "author").Get<string>();
-            var content = input.Children.First(x => x.Name == "content").Get<string>();
+            var created = input.Children.First(x => x.Name == "created").Get<DateTime>();
+
+            // Unrolling any plugins found in [content].
+            content = UnrollPlugins(signaler, content);
 
             // Transforming content and template.
-            var result = template.Replace("![[content]]!", Markdown.ToHtml(content));
+            var result = template.Replace("![[content]]!", content);
             result = result.Replace("![[title]]!", title);
             result = result.Replace("![[author]]!", author);
             result = result.Replace("![[created]]!", created.ToString("dddd d. MMM HH:mm", CultureInfo.InvariantCulture));
@@ -42,5 +48,56 @@ namespace backend.slots
             // Returning results to caller.
             input.Value = result;
         }
+
+        #region [ -- Private helper methods -- ]
+
+        /*
+         * Unrolls plugins referenced in specified content.
+         */
+        string UnrollPlugins(ISignaler signaler, string content)
+        {
+            var result = new StringBuilder();
+            var buffer = new StringBuilder();
+            using (var reader = new StringReader(content))
+            {
+                var line = reader.ReadLine();
+                while(line != null)
+                {
+                    if (line == "![[")
+                    {
+                        /*
+                         * Hyperlambda content, reading until we find the end of it, parsing, 
+                         * evaluating, and adding results of evaluation into buffer.
+                         */
+                        result.Append(Markdown.ToHtml(buffer.ToString()));
+                        buffer.Clear();
+                        line = reader.ReadLine(); // Discarding opening "![[" parts.
+                        while(line != "]]!")
+                        {
+                            buffer.Append(line).Append("\r\n");
+                            line = reader.ReadLine();
+                        }
+                        var lambda = new Parser(buffer.ToString()).Lambda();
+                        buffer.Clear();
+                        var evalResult = new Node();
+                        signaler.Scope("slots.result", evalResult, () =>
+                        {
+                            signaler.Signal("eval", lambda);
+                        });                        
+                        result.Append(evalResult.Get<string>());
+                    }
+                    else
+                    {
+                        // Normal content.
+                        buffer.Append(line).Append("\r\n");
+                    }
+                    line = reader.ReadLine();
+                }
+                result.Append(Markdown.ToHtml(buffer.ToString()));
+            }
+            return result.ToString();
+        }
+
+        #endregion
     }
 }
