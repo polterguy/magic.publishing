04f55c4869b7858777960a09586469c3493efeda
Thomas Hansen <thomas@gaiasoul.com>
2020-02-26 16:53:21 +0200

    Bettering

---
+++ backend/slots/ConvertDate2UTC.cs
@@ -0,0 +1,34 @@
+/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - 2020, thomas@servergardens.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
+ */
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace backend.slots
+{
+    /// <summary>
+    /// [magic.publishing.date.convert2utc] slot for converting a date to a UTC date.
+    /// </summary>
+    [Slot(Name = "magic.publishing.date.convert2utc")]
+    public class ConvertDate2UTC : ISlot
+    {
+        /// <summary>
+        /// Handles the signal for the class.
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal the slot.</param>
+        /// <param name="input">Root node for invocation.</param>
+        /// <returns>An awaitable task.</returns>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            input.Value = input.GetEx<DateTime>().ToUniversalTime();
+        }
+    }
+}
