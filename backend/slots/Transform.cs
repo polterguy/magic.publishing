6238023a0a4a35d84fe81a7350460d4cc95c3179
Thomas Hansen <thomas@gaiasoul.com>
2020-03-01 10:00:25 +0200

    Update Transform.cs

--- backend/slots/Transform.cs
+++ backend/slots/Transform.cs
@@ -32,12 +32,18 @@ namespace backend.slots
         /// <returns>An awaitable task.</returns>
         public async Task SignalAsync(ISignaler signaler, Node input)
         {
-            // Retrieving and unrolling any plugins and arguments found in [template].
+            /*
+             * Retrieving and unrolling any plugins and arguments found in [template].
+             * Notice, the template is assumed to be HTML and not Markdown.
+             */
             var template = input.Children.First(x => x.Name == "template").Get<string>();
             template = UnrollArguments(input, template);
             template = await UnrollPluginsAsync(signaler, template, input, false);
 
-            // Retrieving and unrolling any plugins found in [content].
+            /*
+             * Retrieving and unrolling any plugins and arguments found in [content].
+             * Notice, the template is assumed to be Markdown and not HTML.
+             */
             var content = input.Children.First(x => x.Name == "content").Get<string>();
             content = UnrollArguments(input, content);
             content = await UnrollPluginsAsync(signaler, content, input, true);
@@ -53,6 +59,12 @@ namespace backend.slots
 
         string UnrollArguments(Node node, string content)
         {
+            /*
+             * Substitution of everything except [content], allowing
+             * to extend the SQL query actually selecting the document
+             * with additional fields, to easily extend the substitution
+             * process.
+             */
             foreach (var idx in node.Children.Where(x => x.Name != "content"))
             {
                 content = content.Replace(
@@ -60,66 +72,6 @@ namespace backend.slots
                     Convert.ToString(idx.Value, CultureInfo.CurrentUICulture));
             }
             return content;
-        }
-
-        /*
-         * Unrolls plugins referenced in specified content.
-         */
-        string UnrollPlugins(
-            ISignaler signaler, 
-            string content, 
-            Node node,
-            bool isMarkdown)
-        {
-            var result = new StringBuilder();
-            var buffer = new StringBuilder();
-            using (var reader = new StringReader(content))
-            {
-                var line = reader.ReadLine();
-                while(line != null)
-                {
-                    if (line == "![[")
-                    {
-                        /*
-                         * Hyperlambda content, reading until we find the end of it, parsing, 
-                         * evaluating, and adding results of evaluation into buffer.
-                         *
-                         * Making sure we first append content found so far in StringReader.
-                         */
-                        if (isMarkdown)
-                            result.Append(Markdown.ToHtml(buffer.ToString()));
-                        else
-                            result.Append(buffer.ToString());
-                        buffer.Clear();
-                        line = reader.ReadLine(); // Discarding opening "![[" parts.
-                        while(line != "]]!")
-                        {
-                            buffer.Append(line).Append("\r\n");
-                            line = reader.ReadLine();
-                        }
-                        var lambda = new Parser(buffer.ToString()).Lambda();
-                        lambda.Add(new Node(".arguments", null, node.Children.Select(x => x.Clone())));
-                        buffer.Clear();
-                        var evalResult = new Node();
-                        signaler.Scope(
-                            "slots.result",
-                            evalResult,
-                            () => signaler.Signal("eval", lambda));
-                        result.Append(evalResult.Get<string>());
-                    }
-                    else
-                    {
-                        // Normal content.
-                        buffer.Append(line).Append("\r\n");
-                    }
-                    line = reader.ReadLine();
-                }
-                if (isMarkdown)
-                    result.Append(Markdown.ToHtml(buffer.ToString()));
-                else
-                    result.Append(buffer.ToString());
-            }
-            return result.ToString();
         }
 
         /*
@@ -138,6 +90,7 @@ namespace backend.slots
                 var line = reader.ReadLine();
                 while(line != null)
                 {
+                    // TODO: Implement true parsing, to allow for having plugins inside of e.g. attributes, etc.
                     if (line == "![[")
                     {
                         /*
@@ -157,6 +110,11 @@ namespace backend.slots
                             buffer.Append(line).Append("\r\n");
                             line = reader.ReadLine();
                         }
+
+                        /*
+                         * Notice, passing in all arguments as [.arguments] to all
+                         * inline Hyperlambda plugin sections.
+                         */
                         var lambda = new Parser(buffer.ToString()).Lambda();
                         lambda.Add(new Node(".arguments", null, node.Children.Select(x => x.Clone())));
                         buffer.Clear();
@@ -164,7 +122,12 @@ namespace backend.slots
                         await signaler.ScopeAsync(
                             "slots.result",
                             evalResult,
-                            async () => await signaler.SignalAsync("eval", lambda));
+                            async () => await signaler.SignalAsync("wait.eval", lambda));
+
+                        /*
+                         * Assuming the plugin section returned some sort of inclusion,
+                         * effectively being server side "document.write" logic.
+                         */
                         result.Append(evalResult.Get<string>());
                     }
                     else
